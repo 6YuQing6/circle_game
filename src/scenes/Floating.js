690bbcbe498af691112639af2bf3ea98249c3958
Sunny Han <shan66@ucsc.edu>
2024-11-11 19:48:25 -0800

    adding circle creation

--- src/scenes/Floating.js
+++ src/scenes/Floating.js
@@ -1,49 +1,53 @@
 class Floating extends Phaser.Scene {
-    constructor() {
-      super("FloatScene");
+  constructor() {
+    super("FloatScene");
+  }
+  preload() {
+    this.colors = [0xff0000];
+  }
+  create() {
+    this.circle = this.add.circle(410, 410, 256, 0xff0000);
+    this.physics.add.existing(this.circle);
+    this.circle.body.setBounce(1);
+    this.circle.body.setCollideWorldBounds(true);
+    this.circle.body.setVelocity(100, 100);
+    this.circleGroup = this.add.group({
+      runChildUpdate: true,
+    });
+
+    this.circleGroup.add(this.circle);
+
+    function calculateSpeed(size) {
+      if (size > 64) {
+        return size / 2;
+      } else {
+        return size * 6;
+      }
     }
-    preload() {
-      this.colors = [0xff0000, ]
-    }
-    create(){
-        this.circle = this.add.circle(410, 410, 256, 0xff0000)
-        this.physics.add.existing(this.circle)
-        this.circle.body.setBounce(1)
-        this.circle.body.setCollideWorldBounds(true)
-        this.circle.body.setVelocity(100,100)
-        this.cricleGroup = this.add.group( {
-          runChildUpdate: true,
-        })
-        this.cricleGroup.add(this.cricle)
-        function calculateSpeed(size) {
-          if (size > 64) {
-            return size / 2
-          } else {
-            return size * 6
-          }
+    this.createCircles = (positionX, positionY, size, color) => {
+      for (let i = 0; i < 2; i++) {
+        let temp = this.add.circle(positionX, positionY, size / 2, color);
+        this.physics.add.existing(temp);
+        temp.body.setBounce(1);
+        temp.body.setCollideWorldBounds(true);
+        temp.body.setVelocity(calculateSpeed(size));
+        this.circleGroup.add(temp);
+      }
+    };
+    this.createCircles(100, 100, 100, 0xff0000);
 
-        }
-        function createCircle(positionX, positionY, size, color) {
-          for (let i = 0; i < 2; i++)
-            temp = this.add.circle(positionX, positionY, size/2, color)
-            this.physics.add.existing(circle)
-            this.circle.body.setBounce(1)
-            this.circle.body.setCollideWorldBounds(true)
-            this.circle.body.setVelocity( )
-            this.cricleGroup.add(temp)
-        }
-
-
-        this.physics.add.collider(this.cricleGroup,this.cricleGroup, (circle1, circle2) => {
+    this.physics.add.collider(
+      this.circleGroup,
+      this.circleGroup,
+      (circle1, circle2) => {
         if (circle1.radius == circle2.radius) {
-          circle1.destroy()
-          circle2.destroy()
-          // createCircle(circle1.X, circle1.Y, circle1.size * 2, color)
+          circle1.destroy();
+          circle2.destroy();
+          // createCircles(circle1.X, circle1.Y, circle1.size * 2, color)
         }
-    })
-    }
-    
-    update(){
+      }
+    );
+  }
 
-    }
+  update() {}
 }
