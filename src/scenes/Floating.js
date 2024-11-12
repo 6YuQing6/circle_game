0654f15addf530e504477ab20c45b12d88fba32e
Sunny Han <shan66@ucsc.edu>
2024-11-11 19:07:22 -0800

    adding group

--- src/scenes/Floating.js
+++ src/scenes/Floating.js
@@ -1,15 +1,18 @@
 class Floating extends Phaser.Scene {
-    constructor() {
-      super("FloatScene");
-    }
-    create(){
-        this.circle = this.add.circle(400, 300, 20, 0xff0000)
-        this.physics.add.existing(this.circle)
-        this.circle.body.setBounce(1)
-        this.circle.body.setCollideWorldBounds(true)
-        this.circle.body.setVelocity(100,100)
-    }
-    update(){
+  constructor() {
+    super("FloatScene");
+  }
+  create() {
+    this.circle = this.add.circle(width / 2, height / 2, 250, 0xff0000);
+    this.physics.add.existing(this.circle);
+    this.circle.body.setBounce(1);
+    this.circle.body.setCollideWorldBounds(true);
+    this.circle.body.setVelocity(100, 100);
 
-    }
+    this.circleGroup = this.add.group({
+      runChildUpdate: true,
+    });
+    this.circleGroup.add(this.circle);
+  }
+  update() {}
 }
