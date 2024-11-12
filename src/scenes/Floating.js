3c4375861cf1435d71faaa5b1871bbf6c5442e05
Justin Xu <jxu240@ucsc.edu>
2024-11-11 19:37:03 -0800

    unstable commit

--- src/scenes/Floating.js
+++ src/scenes/Floating.js
@@ -1,20 +1,49 @@
 class Floating extends Phaser.Scene {
-  constructor() {
-    super("FloatScene");
-  }
-  create() {
-    this.circle = this.add.circle(width / 2, height / 2, 250, 0xff0000);
-    this.physics.add.existing(this.circle);
-    this.circle.body.setBounce(1);
-    this.circle.body.setCollideWorldBounds(true);
-    this.circle.body.setVelocity(100, 100);
+    constructor() {
+      super("FloatScene");
+    }
+    preload() {
+      this.colors = [0xff0000, ]
+    }
+    create(){
+        this.circle = this.add.circle(410, 410, 256, 0xff0000)
+        this.physics.add.existing(this.circle)
+        this.circle.body.setBounce(1)
+        this.circle.body.setCollideWorldBounds(true)
+        this.circle.body.setVelocity(100,100)
+        this.cricleGroup = this.add.group( {
+          runChildUpdate: true,
+        })
+        this.cricleGroup.add(this.cricle)
+        function calculateSpeed(size) {
+          if (size > 64) {
+            return size / 2
+          } else {
+            return size * 6
+          }
 
-    this.circleGroup = this.add.group({
-      runChildUpdate: true,
-    });
-    this.circleGroup.add(this.circle);
-  }
-  update() {
-    this.physics.add.collider(this.circleGroup, this.circleGroup);
-  }
+        }
+        function createCircle(positionX, positionY, size, color) {
+          for (let i = 0; i < 2; i++)
+            temp = this.add.circle(positionX, positionY, size/2, color)
+            this.physics.add.existing(circle)
+            this.circle.body.setBounce(1)
+            this.circle.body.setCollideWorldBounds(true)
+            this.circle.body.setVelocity( )
+            this.cricleGroup.add(temp)
+        }
+
+
+        this.physics.add.collider(this.cricleGroup,this.cricleGroup, (circle1, circle2) => {
+        if (circle1.radius == circle2.radius) {
+          circle1.destroy()
+          circle2.destroy()
+          // createCircle(circle1.X, circle1.Y, circle1.size * 2, color)
+        }
+    })
+    }
+    
+    update(){
+
+    }
 }
